ad040c43ca4a893f6af7ddb3cd8a5097923e01b4
Joe74012 <1210764@student.roc-nijmegen.nl>
2026-04-10 09:57:03 +0200

    About pagina gemaakt volgens design en die pagina gelinkt aan de header van alle pagina's

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/about', function () {
     return view('about');
 })->name('about');
 
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
