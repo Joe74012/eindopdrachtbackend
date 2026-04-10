4305de2b598bcf73ad957393300115ddf3530639
Joe74012 <1210764@student.roc-nijmegen.nl>
2026-04-10 09:23:22 +0200

    about pagina naar development branch.

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -46,11 +46,11 @@ public function index()
 
     public function show(string $id)
     {
-        //
+
     }
     public function edit(string $id)
     {
-        //
+
     }
 
     public function update(Request $request, string $id)
